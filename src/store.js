c8f1650aac1177825aa89f69cbf9e7a58942713c
NickDev90 <nickie05021990@gmail.com>
2021-11-30 23:54:24 +0100

    saga added

--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
+import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import apiReducer from "./modules/api/reducer";
+import apiRootSaga from "./modules/api/saga";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ 
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
+        : compose;
 
 const reducers = combineReducers({
     api: apiReducer
 });
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(apiRootSaga);
 
 export default store;
+
+// const createAppStore = () => {
+
+//     const Store = createStore(
+//       reducers,
+//       composeEnhancers(applyMiddleware(sagaMiddleware))
+//     );
+//     sagaMiddleware.run(apiRootSaga);
+//     return Store;
+// }
+
+// export default createAppStore();
+
